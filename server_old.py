dfe17949a5cebd38a6d2762c984484346f40608f
jahnvisethjs <jahnviseth@yahoo.com>
2025-12-30 15:07:17 -0600

    promptChanges

--- server_old.py
+++ server_old.py
@@ -158,6 +158,6 @@ def process_file(file):
 
 if __name__ == "__main__":
     if env == "prod":
-        uvicorn.run(app, host="0.0.0.0", port=8001, ssl_keyfile="proxyai.pem", ssl_certfile="proxyai.crt")
+        uvicorn.run(app, host="0.0.0.0", port=8000, ssl_keyfile="proxyai.pem", ssl_certfile="proxyai.crt")
     else:
-        uvicorn.run(app, host="localhost", port=8001)
+        uvicorn.run(app, host="localhost", port=8000)
--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function ChatInterface() {
   const [aiMessage, setAiMessage] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const url = "http://localhost:8001/"
+  const url = "http://localhost:8000/"
   const handleSubmit = async () => {
     setLoading(true);
     const formData = new FormData();
